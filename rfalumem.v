0a6a0128db90e33cd4e51560168834b19f8d4123
Brian J Gravelle <bgravelle5@gmail.com>
2016-05-20 14:53:08 -0700

    combining RF Mem and ALU

--- rfalumem.v
+++ rfalumem.v
@@ -27,18 +27,6 @@ module RegFileALUMemory #(parameter DATA_WIDTH  = 32, NAME_BITS = 5, CTRL_BITS =
    reg  [DATA_WIDTH-1:0] mem_wd;
    reg  [addr_bits-1:0]  mem_ra, mem_wa;
 
-   always @(*) begin
-      a  <= rd1;
-      wd <= (mem_re) ? mem_rd : c;
-      b  <= (imm_e)  ? imm_d : rd2;
-
-      mem_wd <= rd2;
-      mem_ra <= c;
-      mem_wa <= c;
-   end
-   parameter addr_bits  = 16;
-
-
    Memory #(.ADDR_WIDTH(addr_bits)) foam ( 
      .clk (clk), 
      .rst (mem_rst), 
@@ -52,10 +40,9 @@ module RegFileALUMemory #(parameter DATA_WIDTH  = 32, NAME_BITS = 5, CTRL_BITS =
      .rd  (mem_rd)
    ); 
     
-   registerFile regFile ( 
+   registerFile_2 regFile ( 
      .rst (rst), 
-     .write (write), 
-     .read (read), 
+     .we  (write), 
      .rs1 (rs1), 
      .rs2 (rs2), 
      .ws  (ws),  
@@ -74,20 +61,19 @@ module RegFileALUMemory #(parameter DATA_WIDTH  = 32, NAME_BITS = 5, CTRL_BITS =
      .c_out (c_out) 
    ); 
 
-   always @(posedge clk) begin
-      write <= 0;
-      if (rst) begin
-         ws   <= 0;
-         read <= 0;
-      end else begin
-         ws   <= ws_in;
-         read <= 1;
-      end      
+   always @(*) begin
+      a  <= rd1;
+      b  <= (imm_e)  ? imm_d : rd2;
+      wd <= (mem_re) ? mem_rd : c;
+
+      mem_wd <= rd2;
+      mem_ra <= c;
+      mem_wa <= c;
    end
 
-   always @(negedge clk) begin
-     write <= 1;
-     read  <= 0;
+   always @(posedge clk) begin
+      write <= 1;
+      ws    <= (rst) ? 0 : ws_in;
    end
 
 endmodule
@@ -97,6 +83,10 @@ module tb_RegFileALU;
    parameter name_bit = 5;
    parameter num_regs = 32;
    parameter ctrl_bits = 4;
+
+   localparam AND = 4'b0000, OR  = 4'b0001, ADD = 4'b0010, SUB = 4'b0110, 
+              SLT = 4'b0111, SGE = 4'b0101, NOR = 4'b1100;
+
 
    reg clk, rst, imm_e, mem_rst, mem_we, mem_re, mem_rs, mem_ws; 
    reg [ctrl_bits-1:0] op_in;
@@ -139,51 +129,18 @@ module tb_RegFileALU;
        op_in = 4'b0001;
        imm_d = 5;
        imm_e = 1;
+       mem_re  = 0;
+       mem_we  = 0;
    #10 ws  = 2;
        rs1 = 1;
        rs2 = 0;
        op_in = 4'b0001;
        imm_e = 0;
-   #10 ws  = 6;
-       rs1 = 1;
-       rs2 = 0;
-       op_in = 4'b0001;
-   #10 ws  = 3;
-       rs1 = 0;
-       rs2 = 2;
-       op_in = 4'b0010;
-   #10 ws  = 7;
-       rs1 = 6;
-       rs2 = 6;
-       op_in = 4'b0000;
-   #10 ws  = 4;
-       rs1 = 0;
-       rs2 = 3;
-       op_in = 4'b0110;
-   #10 ws  = 6;
-       rs1 = 7;
-       rs2 = 6;
-       op_in = 4'b0101;
-   #10 ws  = 5;
-       rs1 = 0;
-       rs2 = 2;
-       op_in = 4'b0111; 
-   #10 ws  = 0;
-       rs1 = 0;
-       rs2 = 0;
-       op_in = 4'b0000;
-   #10 ws  = 0;
-       rs1 = 0;
-       rs2 = 0;
-       op_in = 4'b0000;
-   #10 ws  = 0;
-       rs1 = 0;
-       rs2 = 0;
-       op_in = 4'b0000;
+       mem_re  = 0;
+       mem_we  = 0;
 
 
-   $display("Expect 5 in r1, r2, and r3; -5 in r4; 1 in r5");
-   $display("Also 5 in r7; r6 will be 5 then 1");
+   $display("Expect ");
 
    end 
     
